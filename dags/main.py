d01ade1c32a559beb0a88d6f7d5becf279cae856
Mavan <Mavan@MTIN25>
2026-09-01 13:40:02 +0700

    feat: add DAG triggering workflow and update task count tests

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
+
 from api.video_stats import (
     get_playlist_id, 
     get_video_ids, 
@@ -47,8 +50,13 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db"
+    )
+
     #Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task 
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 
 
@@ -59,16 +67,22 @@ with DAG(
     dag_id="update_db",
     default_args= default_args,
     description="DAG to product JSON file and insert data into bioth staging and core schemas",
-    schedule="0 15 * * *",
+    
     catchup=False,
+    schedule=None
 )as dag_update:
 
     #Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality"
+    )
+
     #Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 
 
@@ -77,9 +91,10 @@ with DAG(
     dag_id="data_quality",
     default_args= default_args,
     description="DAG to check the data quanlity on both layers in the db",
-    schedule="0 16 * * *",
+
     catchup=False,
-)as dag_update:
+    schedule=None
+)as dag_quality:
 
     #Define tasks
     soda_validate_staging = yt_elt_data_quality(staging_schema)
